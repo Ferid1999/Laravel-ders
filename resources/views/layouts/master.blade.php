344d70b9144518fba04636b516d7a2b47d2c1506
Ferid Ehmedov <feridehmedov1999@gmail.com>
2018-09-29 09:34:35 +0400

    ver7

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title',config('app.name'))</title>
+    @include('layouts.partials.head')
+    @yield('head')
+</head>
+
+<body id="commerce">
+    
+@include('layouts.partials.navbar')
+
+@yield('content')
+@include('layouts.partials.footer')
+   
+    @yield('footer')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+
+</html>
