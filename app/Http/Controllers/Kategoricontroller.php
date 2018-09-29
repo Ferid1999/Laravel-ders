24784c6cdf26ed31011deeeb539e3c0688315abf
Ferid Ehmedov <feridehmedov1999@gmail.com>
2018-09-29 10:31:19 +0400

    ver9

--- app/Http/Controllers/Kategoricontroller.php
+++ app/Http/Controllers/Kategoricontroller.php
@@ -1,1 +1,14 @@
-Kategoricontroller.php
+<?php  
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Kategoricontroller extends Controller
+{
+    public function index(){
+    	return view('kategori');
+ 
+    }
+}
+
+?>
